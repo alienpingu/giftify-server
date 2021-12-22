548e14e789c1148216c97ef0d6ecaeddac7494cc
alienpingu <samuele.bertocco@gmail.com>
2021-12-22 12:01:10 +0100

    fix env port

--- index.js
+++ index.js
@@ -46,6 +46,6 @@ app.post('/get', (req, res) => {
 })
 
 
-app.listen(port, () => {
-  console.log(`Server starting on port: ${port} ...`)
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
