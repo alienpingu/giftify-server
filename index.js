53b04c4d8b22ba9fcd35579b76b18109dca0a49c
alienpingu <samuele.bertocco@gmail.com>
2021-12-22 12:15:44 +0100

    allow all

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const port = 5000
-app.use(express.json()); // built-in middleware for express
 
 const { Pool, Client } = require('pg')
 
@@ -19,12 +18,38 @@ const client = new Client({
         rejectUnauthorized: false,
     },
 })
+
+app.use(express.json()); // built-in middleware for express
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 client.connect()
 
+
+//Get req
 app.get('/', (req, res) => {
 	res.status(200).send('ok')
-	
+
 })
+
+//Post req
 
 app.post('/save', (req, res) => {
 	console.log(req.body)
