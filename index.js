b66b788bd1963cce1c04782f2c96db2991e1b1fd
alienpingu <samuele.bertocco@gmail.com>
2021-12-22 12:09:11 +0100

    remove cors add express

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const express = require('express')
 const app = express()
-var cors = require('cors')
 const port = 5000
+app.use(express.json()); // built-in middleware for express
+
 const { Pool, Client } = require('pg')
+
 require("dotenv").config();
 const postgresURL = process.env.postgresURL;
+
 const connectionString = postgresURL
 const pool = new Pool({
   connectionString,
 })
+
 const client = new Client({
   	connectionString: connectionString,
 	ssl: {
         rejectUnauthorized: false,
     },
 })
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.use(cors())
-
 client.connect()
 
 app.get('/', (req, res) => {
 	res.status(200).send('ok')
+	
 })
 
 app.post('/save', (req, res) => {
